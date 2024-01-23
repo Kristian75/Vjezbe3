74d322ed0ba57cf050a52b82989272d1fe54e6d3
Kristian75 <152974653+Kristian75@users.noreply.github.com>
2024-01-23 13:00:45 +0100

    Add files via upload

---
+++ zadatak10.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova:");
+            string nizZnakova = Console.ReadLine();
+
+            int[] brojac = new int[256];
+
+            foreach (char znak in nizZnakova)
+            {
+                brojac[znak]++;
+            }
+
+            int maxBrojac = 0;
+            char najcesciZnak = ' ';
+
+            for (int i = 0; i < 256; i++)
+            {
+                if (brojac[i] > maxBrojac)
+                {
+                    maxBrojac = brojac[i];
+                    najcesciZnak = (char)i;
+                }
+            }
+
+            Console.WriteLine("\nNajcesci znak: " + najcesciZnak);
+            Console.WriteLine("Broj ponavljanja: " + maxBrojac);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zadatak11.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite prvi broj:");
+            int prviBroj = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite drugi broj:");
+            int drugiBroj = int.Parse(Console.ReadLine());
+
+            int najveciDjeljiv = 1;
+
+            for (int i = 1; i <= prviBroj && i <= drugiBroj; i++)
+            {
+                if (prviBroj % i == 0 && drugiBroj % i == 0)
+                {
+                    najveciDjeljiv = i;
+                }
+            }
+
+            Console.WriteLine("Najveci broj s kojim su oba broja djeljiva: " + najveciDjeljiv);
+
+            Console.ReadKey();
+        }
+    }
+}
